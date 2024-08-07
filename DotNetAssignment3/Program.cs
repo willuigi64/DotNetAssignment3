eff6b68354a186de1bacd1c0b9e4f25534304446
willuigi64 <willuigi64@gmail.com>
2024-08-07 14:04:42 -0400

    Add Google 0Auth

--- DotNetAssignment3/Program.cs
+++ DotNetAssignment3/Program.cs
@@ -15,6 +15,17 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+//enable Google oauth before app starts
+//access config from appsettings.json
+var configuration = builder.Configuration;
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+{
+    options.ClientId = configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
